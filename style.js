62e8e16ef3fccfeb2e603052e0aef2b96cb4bbcd
ahmdbabker <83309413+ahmdbabker@users.noreply.github.com>
2022-01-31 18:30:56 +0400

    Add files via upload

---
+++ style.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector(".m-nav .taking-all")
+let togle = document.querySelector(".m-nav .togle")
+
+togle.addEventListener("click" , function(){
+    menu.classList.toggle("activate")
+})
